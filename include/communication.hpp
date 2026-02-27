fee16bacc9200097eac137e6395450aab3ead743
Zai_Hui <2268670394@qq.com>
2026-02-27 14:58:53 +0800

    feat:初始化通信模块

---
+++ include/communication.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Zai_Hui on 2026/2/27.
+//
+
+#ifndef ANALOG_SENSOR_COMMUNICATION_HPP
+#define ANALOG_SENSOR_COMMUNICATION_HPP
+#include <QObject>
+
+class Communication : public QObject {
+    Q_OBJECT
+private:
+
+
+};
+
+#endif //ANALOG_SENSOR_COMMUNICATION_HPP
