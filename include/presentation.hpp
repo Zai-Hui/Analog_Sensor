fa3cd8ff9f781dcbe1eb000aa3f2c0352a0c45cf
Zai_Hui <2268670394@qq.com>
2026-03-06 17:36:10 +0800

    feat:实现解码模块中modbus和csv对应函数定义

--- include/presentation.hpp
+++ include/presentation.hpp
@@ -76,11 +76,72 @@ public:
 private:
     // 处理modbus格式的数据
     void decodeByModbus(const QByteArray& data) {
-
+        _dataModbus.clear();
+        int index = 0;
+        _host = data[index++];
+        _funcCode = data[index++];
+        _addr = static_cast<unsigned char>(data[index++]);
+        _addr = (_addr << 8) | data[index++];
+        _size_oneData = static_cast<unsigned char>(data[index++]);
+        _size_oneData = (_size_oneData << 8) | data[index++];
+        if (_funcCode == 0x0F || _funcCode == 0x10) {
+            _len = data[index++];
+            for (int i = 0; i < _len; i++) {
+                _dataModbus.push_back(data[index++]);
+            }
+        }
     }
     // 处理csv格式的数据
-    void decodeByCsv(const QByteArray& data) {
-
+    void decodeByCsv(const QByteArray& BData) {
+        QString data(BData);
+        // 处理 \r\n 的情况
+        QStringList stringList = data.split("\n");
+        while (stringList.back().isEmpty()) {
+            stringList.pop_back();
+        }
+        for (auto & e : stringList) {
+            if (e.back() == "\r") e.chop(1);
+        }
+        // 获取表格列名
+        QStringList columnsName = getLine(stringList[0]);
+        // 建立列名和列数的映射
+        for (int i = 0; i < columnsName.size(); i++) {
+            if (columnsName[i] != "") { _hash.insert(columnsName[i], i); }
+        }
+        // 将数据存到_dataCsv中
+        for (int i = 1; i < stringList.size(); i++) {
+            QStringList lineData = getLine(stringList[i]);
+            _dataCsv.append(lineData);
+        }
+    }
+    // 处理一行数据
+    QStringList getLine(const QString& str) {
+        int index = 0;
+        QStringList stringList;
+        while (index < str.size() && str[index] != '\n') {
+            stringList.append("");
+            int beginIndex = index;
+            while (index < str.size() && str[index] != ',' && str[index] != '\n') {
+                if (str[index] == '\"') {
+                    index++;
+                    while (str[index] != '\"')
+                        index++;
+                }
+                index++;
+            }
+            if (index > 0 && str[index - 1] == '\"') {
+                stringList.back() += str.mid(beginIndex + 1, index - beginIndex - 2);
+            }
+            else if (index == 0 || str[index - 1] == ',') {
+                index++;
+                continue;
+            }
+            else {
+                stringList.back() += str.mid(beginIndex, index - beginIndex);
+            }
+            index++;
+        }
+        return stringList;
     }
     // 处理json格式的数据
     void decodeByJson(const QByteArray& data) {
@@ -117,6 +178,16 @@ public:
     func* processByModbus = nullptr;
     func* processByCsv = nullptr;
     func* processByJson = nullptr;
+    // modbus协议成员
+    uint8_t _host = 0x00;               // 从机地址
+    uint8_t _funcCode = 0x00;           // 功能码
+    uint16_t _addr = 0x0000;            // 数据地址
+    uint16_t _size_oneData = 0x0000;    // 目标数量/单个数据
+    uint8_t _len = 0x00;                // 字节计数
+    std::vector<uint8_t> _dataModbus;   // 字节数据
+    // csv格式成员
+    QMap<QString, int> _hash;
+    QVector<QStringList> _dataCsv;
 };
 
 #endif //ANALOG_SENSOR_PRESENTATION_HPP
